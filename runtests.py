247e8bf04af3232bddbf00ebc6129ec22b88c22b
Mario Orlandi <morlandi@brainstorm.it>
2020-09-22 10:05:43 +0200

    verbose test runner

--- runtests.py
+++ runtests.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+    test_runner = TestRunner(verbosity=2)
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
